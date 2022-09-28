f836bbca6b5281fcf23f9ee8eab57c6497fa9167
Parallel Works User Account <sfg4@canary-user-01.parallel.works>
2022-09-28 19:32:21 +0000

    Updated draft of convert_colab
    Still need to get a cell-by-cell filter to remove the installation
    of dependencies.  May need to do this in raw JSON or in Python
    see these links for hints:
    Adding cell tags: https://jupyterbook.org/en/stable/content/metadata.html
    Filter whole cells: https://nbconvert.readthedocs.io/en/latest/removing_cells.html
    If we don't want to filter anything: https://github.com/googlecolab/colabtools

--- colabfold/convert_colab.sh
+++ colabfold/convert_colab.sh
@@ -6,6 +6,9 @@
 # TODO: Eventually, consider using Colab
 # parameter markup information to build
 # a workflow.xml file to run interations.
+# A nice Colab tutorial with an interactive
+# demo of the different input types is here:
+# https://colab.research.google.com/notebooks/io.ipynb
 #==================
 
 # Specify the name of the Colab.ipynb
@@ -18,7 +21,16 @@ current_dir=`pwd`
 bn=`basename $colab_ipynb .ipynb`
 
 # Convert notebook code to python code
-jupyter nbconvert --log-level 0 --to script --output ${current_dir}/${bn} $colab_ipynb
+# Drop the cell with the title "Install dependencies"
+# Unfortunately, this preprocessor only grabs contents
+# of cells and not whole cells!  WORKING HERE
+jupyter nbconvert --log-level 0 --to script --output ${current_dir}/${bn}.tmp --RegexRemovePreprocessor.patterns="Install.dependencies" $colab_ipynb
+
+# Additional things that we can remove:
+# 1) google.colab's files tool for uploading data to notebook
+# 2) Google Drive usage
+grep -iv drive ${current_dir}/${bn}.tmp.py | grep -iv uploaded | grep -iv google.colab | grep -iv gauth | grep -iv GoogleCredentials | grep -iv auth.authenticate_user > ${current_dir}/${bn}.py
+
 
 #===========================
 # Working here to PoC conversion
